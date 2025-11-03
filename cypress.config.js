5a5a2405d7c2e3659d8ac4e79ca1141fb094ee58
Jailson Pereira <89271671+jaipereirastech@users.noreply.github.com>
2025-11-03 08:13:18 -0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -6,7 +6,7 @@ module.exports = defineConfig({
     runMode: 2
   },
   e2e: {
-    defaultCommandTimeout: 10000, //Definindo timeout para o caso 15
+    defaultCommandTimeout: 120000, //Definindo timeout para o caso 15
     reporter: 'cypress-mochawesome-reporter',
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
